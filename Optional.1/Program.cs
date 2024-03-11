c5958c718250c779fb7cea7134a48a4085587610
Denys Brahin <denisbragin52@gmail.com>
2024-03-11 13:55:21 +0200

    First commit

---
+++ Optional.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+public interface IMyList<in T>
+{
+    void Add(T t);
+}
+
+public class MyList<T> : IMyList<T>, IEnumerable
+{
+    private List<T> list = new List<T>();
+    public void Add(T t)
+    {
+        list.Add(t);
+    }
+
+    public T this[int index]
+    {
+        get { return list[index]; }
+        set { list[index] = value; }
+    }
+
+    public int Count { get { return list.Count; } }
+
+    public IEnumerator GetEnumerator()
+    {
+        return list.GetEnumerator();
+    }
+}
+
+
+internal class Program
+{
+    private static void Main()
+    {
+        var list = new MyList<int>();
+
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+        list.Add(5);
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.WriteLine(list[i]);
+        }
+
+        foreach (var l in list)
+        {
+            Console.WriteLine(l);
+        }
+    }
+}
+
